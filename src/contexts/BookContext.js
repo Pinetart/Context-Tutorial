fbcf2ce325073f0e31bce7a0f055d54805aaea02
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-03-23 14:33:08 -0400

    Reading List App (part 2)

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,25 +1,25 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/v1';
+import React, { createContext, useState } from "react";
+import uuid from "uuid/v1";
 
 export const BookContext = createContext();
 
-const BookContextProvider = (props) => {
+const BookContextProvider = ({ children }) => {
   const [books, setBooks] = useState([
-    {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-    {title: 'the final empire', author: 'brandon sanderson', id: 2},
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brandon sanderson", id: 2 },
   ]);
   const addBook = (title, author) => {
-    setBooks([...books, {title, author, id: uuid()}]);
+    setBooks([...books, { title, author, id: uuid() }]);
   };
   const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
-  }
+    setBooks(books.filter((book) => book.id !== id));
+  };
 
   return (
     <BookContext.Provider value={{ books, addBook, removeBook }}>
-      {props.children}
+      {children}
     </BookContext.Provider>
   );
-}
- 
+};
+
 export default BookContextProvider;
